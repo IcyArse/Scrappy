63e46b45d183496720158712f010fa624e46634f
Icy <yatharthsaroha2003@gmail.com>
2024-04-21 11:42:39 +0530

    update

--- maina.py
+++ maina.py
@@ -17,7 +17,7 @@ class_id = 43513647#input("Enter the class ID: ")
 # Set Chrome options
 chrome_options = Options()
 #chrome_options.add_argument("--disable-infobars")
-#chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
